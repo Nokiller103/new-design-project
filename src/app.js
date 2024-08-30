e4e0bb5acd989213984f00241fc490869270a1c4
Nokiller103 <indikidanime@gmail.com>
2024-08-30 06:47:44 +0000

    add nodemon and style.css

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const path = require ('path');
 const app = express ();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req,res) => {
     //res.send('hello world!');
     res.sendFile(path.join(__dirname, 'pages/index.html'));
